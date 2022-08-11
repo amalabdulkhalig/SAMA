361e59abb9787c5392c8680184a1d0f09b1f708c
amalabdulkhalig <amal4gd@gmail.com>
2022-08-11 13:45:32 +0300

    new file

--- dev/lstm.py
+++ dev/lstm.py
@@ -352,10 +352,9 @@ if __name__ == "__main__":
 
 
 
-    #x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
-    #trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
-    x = torch.tensor(X_train).to(device)
-    trg = torch.tensor(y_train).to(device)
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
+    trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+
 
     src_pad_idx = 0
     trg_pad_idx = 0
